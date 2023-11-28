0dedd42b7c6ec90d13c39903efebf8831b72e986
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-27 23:17:26 -0500

    PL2: Use 2nd arch from common as well
    Change-Id: Iaae7af9eff5d46ec10a7b6f6309bbfee9e348ccd
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ DEVICE_PATH := device/nokia/PL2
 
 # Architecture
 TARGET_CPU_VARIANT_RUNTIME := cortex-a53
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
 
 # Assert
 TARGET_BOARD_INFO_FILE := device/nokia/PL2/board-info.txt
