Nokia-SDM660-Devices/android_device_nokia_PL2

PL2: Implement Retrofit Dynamic Partitions [1/2]
refs: https://source.android.com/docs/core/ota/dynamic_partitions/implement
refs: https://github.com/log1cs/android_device_nokia_NLA/commit/92a1998ca00f7209592a66b9b59b7018eb46ea29

Signed-off-by: userariii <abhattacharjee717@gmail.com>

BoardConfig.mk
@@ -37,10 +37,16 @@ TARGET_KERNEL_CONFIG := nokia_defconfig
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
-BOARD_VENDORIMAGE_PARTITION_SIZE := 1073741824
 BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_SUPER_PARTITION_BLOCK_DEVICES := system vendor
+BOARD_SUPER_PARTITION_METADATA_DEVICE := system
+BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE := 3221225472
+BOARD_SUPER_PARTITION_VENDOR_DEVICE_SIZE := 1073741824
+BOARD_SUPER_PARTITION_SIZE := $(shell expr $(BOARD_SUPER_PARTITION_VENDOR_DEVICE_SIZE) + $(BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE) )
+BOARD_SUPER_PARTITION_GROUPS := nokia_sdm660_dynamic_partitions
+BOARD_NOKIA_SDM660_DYNAMIC_PARTITIONS_SIZE := $(shell expr $(BOARD_SUPER_PARTITION_SIZE) - 4194304 )
+BOARD_NOKIA_SDM660_DYNAMIC_PARTITIONS_LIST := system vendor
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop

PixelExperience-Devices/device_nokia_PL2

BoardConfig.mk
@@ -37,10 +37,16 @@ TARGET_KERNEL_CONFIG := nokia_defconfig
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
-BOARD_VENDORIMAGE_PARTITION_SIZE := 1073741824
 BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_SUPER_PARTITION_BLOCK_DEVICES := system vendor
+BOARD_SUPER_PARTITION_METADATA_DEVICE := system
+BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE := 3221225472
+BOARD_SUPER_PARTITION_VENDOR_DEVICE_SIZE := 1073741824
+BOARD_SUPER_PARTITION_SIZE := $(shell expr $(BOARD_SUPER_PARTITION_VENDOR_DEVICE_SIZE) + $(BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE) )
+BOARD_SUPER_PARTITION_GROUPS := nokia_sdm660_dynamic
+BOARD_NOKIA_SDM660_DYNAMIC_SIZE := $(shell expr $(BOARD_SUPER_PARTITION_SIZE) - 4194304 )
+BOARD_NOKIA_SDM660_DYNAMIC_PARTITION_LIST := system vendor
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop